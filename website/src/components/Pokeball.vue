a9fd853ed9fcb3fccf7d316cf78d0f7bffd6de74
Piratheeban <piratheeban95@hotmail.com>
2019-03-06 02:29:49 -0500

    working out animation

---
+++ website/src/components/Pokeball.vue
@@ -0,0 +1,138 @@
+<template>
+<div>
+<div v-click-outside="hide" class="pokeball">
+  <div class="top">
+    <div class="middle_circle">
+        <div class="white_in"></div>
+    </div>
+  </div>
+  <div class="bottom"></div>
+</div>
+</div>
+</template>
+
+<script>
+    import ClickOutside from 'vue-click-outside'
+
+    export default {
+        name: 'Pokeball',
+        components: {
+        },
+        props: ['pokemon', 'onclick', 'visible'],
+        data(){
+            return{}
+        },
+        beforeMount(){
+            console.log(this.visible);
+            console.log("I dont know wat im doign");
+
+        },
+        methods: {
+            hide(){
+                this.visible = false
+                console.log(this.visible);
+            }
+        },
+        directives: {
+            ClickOutside
+        }
+    };
+</script>
+
+<style>
+@font-face {
+  font-family: 'Spinnaker';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Spinnaker'), local('Spinnaker-Regular'), url(http://themes.googleusercontent.com/static/fonts/spinnaker/v5/WxzDAY6mC9v3znSJEtCoWz8E0i7KZn-EPnyo3HZu7kw.woff) format('woff');
+}
+
+body{
+    overflow-y: hidden;
+}
+
+
+.pokeball{
+  margin:50px auto;
+  animation: spin 1.8s infinite linear;
+  margin-bottom: 25%;
+}
+
+div {
+  margin: 0 auto; padding: 0;
+}
+
+.top{
+  background-color:#EB2224;
+  height:110px;
+  width:230px;
+  border: solid black 0.5px;
+  border-radius:195px 195px 0 0;
+  border-bottom:6px solid #000000;
+}
+
+/*.middle{
+  background-color:#000000;
+  width:230px;
+  height:1px;
+  border:4px solid #000000;
+}*/
+
+.middle_circle{
+  background-color:#FFFFFF;
+  width:75px;
+  height:75px;
+  border-radius:50%;
+  margin: 70px 65px;
+  position:absolute;
+  border:12px solid #000000;
+}
+
+.white_in{
+  background-color:#FFFFFF;
+  width:50px;
+  height:50px;
+  border-radius:50%;
+  padding:4px 4px;
+  margin:5px 6px;
+  border:3px solid #000000;
+  -webkit-animation:myfirst 5s; /* Chrome, Safari, Opera */
+  animation:myfirst 4s infinite linear;
+}
+
+.bottom{
+  background-color:#FFFFFF;
+  height:110px;
+  width:230px;
+  border-radius:0 0 195px 195px;
+  border-top:6px solid #000000;
+  border-bottom: solid black 0.5px;
+}
+
+/* Animations */
+
+@keyframes spin {
+	0%  {transform: rotate(0deg);}
+	50%{transform: rotate(40deg);}
+   100%{transform: rotate(0deg);}
+}
+
+/* Chrome, Safari, Opera */
+@-webkit-keyframes myfirst
+{
+0%   {background: white;}
+25%   {background: #db9090;}
+50%   {background: white;}
+75%  {background: #db9090;}
+}
+
+/* Standard syntax */
+@keyframes myfirst
+{
+0%   {background: white;}
+25%   {background: #db9090;}
+50%   {background: white;}
+75%  {background: #db9090;}
+}
+
+</style>
--- website/src/components/Pokemon.vue
+++ website/src/components/Pokemon.vue
@@ -3,24 +3,70 @@
 <button v-on:click="showDetails()">
 <h1>{{ pokemon.name }}</h1>
 </button>
+<Pokeball v-if="visibility" :visible="visibility" />
+<!-- <vodal :show="show" animation="rotate" @hide="show = visibility"></vodal> -->
 </div>
 </template>
 
 <script>
-    import axios from 'axios';
+    import axios from 'axios'
+    import Vodal from 'vodal'
+    import swal from 'sweetalert2'
+    import Pokeball from './Pokeball.vue'
+
     export default {
         name: 'Pokemon',
+        components: {
+            Vodal,
+            Pokeball,
+        },
         props: ['pokemon', 'onclick'],
+        data(){
+            return{
+                visibility : false,
+            }
+        },
         methods: {
+            openPokemon: function() {
+                this.visibility = true
+            },
+            openModel: function(params) {
+                //console.log(params);
+                let megaString = '<div class="height-weight-container"><div class="height-container"><h3>Height: </h3>' + params.height + ' inches</div>';
+                megaString += '<div class="weight-container"><h3>Weight: </h3>' + params.weight + ' pounds</div></div>';
+                megaString = megaString + '</div><br /><h3>Types:</h3>';
+                params.types.forEach((element, index) => {
+                    //console.log(element.type.name);
+                    megaString += element.type.name;
+                    if(params.types.length-1 !== index ){
+                        megaString += ', ';
+                    }
+                });
+                swal.fire({
+                     title: params.name,
+                     html: megaString,
+                     confirmButtonText: 'Cool'
+                })
+            },
             showDetails: function(){
                 axios.get(`http://127.0.0.1:5000/pokemons?pokemon=${this.pokemon.name}`)
                 .then(response => {
-                    const {id, abilities, moves, types, height, weight} = response.data;
-                    console.log(id);
-                    abilities.forEach(element => {
-                        console.log(element.ability.name);
-                    });
-                    //console.log(response.data);
+                    const {id, name, abilities, moves, types, height, weight} = response.data;
+                    // console.log(id);
+                    // console.log(name);
+                    // abilities.forEach(element => {
+                    //     console.log(element.ability.name);
+                    // });
+                    // moves.forEach(element => {
+                    //     console.log(element.move.name);
+                    // });
+                    // types.forEach(element => {
+                    //     console.log(element.type.name);
+                    // });
+                    // console.log(height + " inches");
+                    // console.log(weight + " pounds");
+                    //this.openModel(response.data);
+                    this.openPokemon();
                 })
                 .catch(e => {
                     console.log(e);
@@ -33,5 +79,31 @@
 <style>
     .container-pokedex button{
         border: none;
+    }
+    .height-container, .weight-container{
+        width: 100%;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: space-evenly;
+    }
+    .height-weight-container{
+        display: flex;
+        flex-flow: row nowrap;
+    }
+    .moves-container{
+        width:100%;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-evenly;
+    }
+    .moves-container p{
+        width: 33%;
+        margin-bottom:-5%;
+    }
+    .swal2-show{
+        width: 500px;
+        height: 500px;
+        border-radius: 50%;
+        background-color: red;
     }
 </style>
